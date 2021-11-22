f5f6c960792f9bed8933859ac1004f03e72ea51d
uozcan12 <ugur_ozcan_108@hotmail.com>
2021-11-22 23:14:39 +0300

    added templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
+
+def all_products(request):
+    products =  Product.objects.all()
+    context = {"products":products}
+    return render(request, "store/home.html", context=context)
